c7b429a65e416d9fce25ce128a9e070eb5183470
Simon Ratner <simon+github@probablyprime.net>
2011-03-14 05:09:30 -0700

    implement in-polygon test

--- tojki.clj
+++ tojki.clj
@@ -1,4 +1,5 @@
 (defn make-grid
+  "Create a grid."
   ([w h]
     (make-grid w h 0))
   ([w h initial-value]
@@ -13,10 +14,24 @@
 (defn set-grid [g x y v]
   (assoc-in g [y x] v))
 
-(defn neighbours [g x y]
-  (let [x- (max 0 (dec x))
-        x+ (min (count (first g)) (+ x 2))
-        y- (max 0 (dec y))
-        y+ (min (count g) (+ y 2))]
-    (vec (map #(subvec % x- x+)
-               (subvec g y- y+)))))
+(defn neighbours
+  "Returns sub-grid containing immediate neighbours of [x y]."
+  ([g x y]
+    (let [x- (max 0 (dec x))
+          x+ (min (count (first g)) (+ x 2))
+          y- (max 0 (dec y))
+          y+ (min (count g) (+ y 2))]
+      (vec (map #(subvec % x- x+)
+                 (subvec g y- y+))))))
+
+(defn in-polygon
+  "Test point is within a polygon if when projected on the y-axis it's x value
+  is below odd number of polygon edges; works for convex and concave polygons.
+  @see http://alienryderflex.com/polygon/."
+  ([poly x y]
+    (odd?
+      (let [edges (partition 2 1 (conj poly (first poly)))]
+        (apply + (map (fn [[[x1 y1] [x2 y2]]]
+          (if (and (not= (> y1 y) (> y2 y))
+                   (< x (+ (/ (* (- x2 x1) (- y y1)) (- y2 y1)) x1)))
+            1 0)) edges))))))
